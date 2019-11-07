30749e2bccefcd885ae79b79d01b3a58ad865350
InJoon-L <53824954+InJoon-L@users.noreply.github.com>
2019-11-07 20:47:38 +0900

    Add files via upload
    2019/11/07

---
+++ src/YJYC/Quiz_19.java
@@ -0,0 +1,69 @@
+package inJo;
+
+import java.util.Arrays;
+import java.util.Scanner;
+//띄어쓰기 없는 버젼
+public class Quiz_19 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+
+		System.out.println("남학생 수를 입력하세요");
+		int		inputValue	= scn.nextInt();
+		int		caseCount	= 1;
+		//경우의 수 계산
+		for (int i = inputValue; i > 0; i--) {
+			caseCount *= i;
+		}
+		int[]	resultMans	= new int[caseCount];
+		int[][]	mans		= new int[caseCount][inputValue];
+		//비교하기 위한 것 
+		System.out.println("발생 가능한 경우의 수: " + caseCount);
+
+		for (int i = 0; i < caseCount; i++) {
+			//랜덤으로 넣기
+			for (int j = 0; j < inputValue; j++) {
+				mans[i][j]		= (int)(Math.random() * inputValue) + 1;
+
+				//중복 제거
+				for (int z = 0; z < j; z++) {
+					if (z != j && mans[i][z] == mans[i][j]) {
+						j--;
+						break;
+					}
+				}
+			}
+
+			//중복 제거 
+			for (int j = 0; j < i; j++) {
+				//비교 횟수를 위한 임시 변수
+				int tmp = 0;
+				for (int z = 0; z < inputValue; z++) {
+					if (i != j && mans[i][z] == mans[j][z]) tmp++;
+					if (tmp == inputValue) {
+						i--;
+						break;
+					}
+				}
+				if (tmp == inputValue) break;
+			}
+		}
+
+		//정렬
+		for (int i = 0; i < caseCount; i++) {
+			String tmp = "";
+			for (int j = 0; j < inputValue; j++) {
+				tmp += mans[i][j];
+			}
+			resultMans[i] = Integer.parseInt(tmp);
+		}
+		
+		Arrays.sort(resultMans);
+		
+		//출력
+		for (int i = 0; i < caseCount; i++) {
+			System.out.println(resultMans[i]);
+		}
+
+		scn.close();
+	}
+}
